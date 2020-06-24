315d19b317834644c28b36f3a648d54ea742745f
Leurs Dimitri <dimitri.leurs@nokia.com>
2020-06-24 22:30:39 +0200

    Starting md5

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -8,7 +8,9 @@ todoRoutes.get('/', async (_, res, __) =>
     console.log(`Request received at "/"`);
     let todos: Array<Todo> = await Todo.getAll()
     //console.log(todos[0].toString());
-    res.status(200).send(`<h1>All your todos</h1>` + showTodos(todos) + `<h1>Add a todo</h1> <form action="/todo" method="POST"><input type="text" name="title"><button type="submit">Add Todo</button></form>`);
+    const addTodoForm: string = `<h1>Add a todo</h1> <form action="/todo" method="POST"><input type="text" name="title"><button type="submit">Add Todo</button></form>`;
+    const md5Form: string = `<h1>Md5 hash</h1> <form action="/hash" method="POST"><input type="text" name="message"><button type="submit">Hash</button></form>`;
+    res.status(200).send(`<h1>All your todos</h1>` + showTodos(todos) + addTodoForm + md5Form);
 })
 
 todoRoutes.post('/todo', async (req, res, __) =>
